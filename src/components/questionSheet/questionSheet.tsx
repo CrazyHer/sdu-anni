ab9153fa8b56e460098a8ca1bc370e461a31e1c3
HeRui <877955100@qq.com>
2021-10-05 14:21:39 +0800

    答题逻辑已完善

--- src/components/questionSheet/questionSheet.tsx
+++ src/components/questionSheet/questionSheet.tsx
@@ -1,7 +1,8 @@
 import { View, Text, Button } from "@tarojs/components";
-import { FC, useState } from "react";
+import { FC, useEffect, useState } from "react";
+import Taro from "@tarojs/taro";
 import { Models } from "src/rapper";
-import { AtButton } from "taro-ui";
+import { AtButton, AtMessage, AtModal, AtToast } from "taro-ui";
 import Style from "./questionSheet.module.css";
 
 const QuestionSheet: FC<{
@@ -13,11 +14,61 @@ const QuestionSheet: FC<{
   // 当前作答的题目
   const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
 
-  // 字典中有选项代表已回答，值为false回答错误,true回答正确；若没有该选项则未回答
-  const [optionStatus, setOptionStatus] = useState<Record<string, boolean>>({});
+  // 选项状态，回答正确、错误、未回答
+  const [optionStatus, setOptionStatus] = useState<{
+    option: string;
+    status: "right" | "wrong" | "none";
+  }>({ option: "", status: "none" });
+
+  // 答题正确则显示下一题按钮
+  const [showNext, setShowNext] = useState(false);
+
+  const [isFinished, setFinished] = useState(false);
+
+  useEffect(() => {
+    // 题目已答完
+    if (
+      !props.questions[currentQuestionIndex] ||
+      props.questions.length ===
+        props.questions.filter(sv => sv.question_status).length
+    ) {
+      setFinished(true);
+      setOptionStatus({
+        option: "",
+        status: "none"
+      });
+      setShowNext(false);
+      return;
+    }
+    // 题目未答或只答了一部分时，更新选项状态
+    if (props.questions[currentQuestionIndex].question_status) {
+      setOptionStatus({
+        option: props.questions[currentQuestionIndex].question_answer,
+        status: "right"
+      });
+      setShowNext(true);
+    } else {
+      setOptionStatus({
+        option: "",
+        status: "none"
+      });
+      setShowNext(false);
+    }
+  }, [currentQuestionIndex]);
+
+  const handleNext = () => {
+    setCurrentQuestionIndex(currentQuestionIndex + 1);
+  };
 
   return (
     <View className={`${Style.content} ${props.className}`}>
+      <AtToast
+        isOpened={isFinished}
+        status="success"
+        onClick={() => Taro.redirectTo({ url: "/pages/process/process" })}
+        text={`恭喜你完成答题
+       可点击返回主页`}
+      />
       <Text>{props.questions[currentQuestionIndex]?.question_name}</Text>
       <View className={Style.options}>
         {props.questions[currentQuestionIndex]?.question_option.map(v => (
@@ -25,42 +76,47 @@ const QuestionSheet: FC<{
             key={v.option}
             // 由题目回答状态设定相应样式
             className={`${Style.btn} ${
-              optionStatus[v.option] === undefined
-                ? ""
-                : optionStatus[v.option]
-                ? Style.right
-                : Style.wrong
-            }`}
+              optionStatus.option === v.option
+                ? optionStatus.status === "right"
+                  ? Style.right
+                  : optionStatus.status === "wrong"
+                  ? Style.wrong
+                  : ""
+                : ""
+            } `}
             onClick={async () => {
+              // 若该题已回答正确，则不响应点击事件
+              if (optionStatus.status === "right") {
+                return;
+              }
               if (
                 v.option ===
                 props.questions[currentQuestionIndex].question_answer
               ) {
-                // 回答正确
-                await props.onFinish(
+                // 回答正确，调用回调函数
+                props.onFinish(
                   props.questions[currentQuestionIndex].question_id
                 );
-                // 还原所有选项的错误状态并设置为唯一正确状态
-                const temStatus = {};
-                temStatus[v.option] = true;
-                setOptionStatus(temStatus);
-                // 两秒后跳转至下一题
-                setTimeout(
-                  () => setCurrentQuestionIndex(currentQuestionIndex + 1),
-                  2000
-                );
+                // 将选项设置为正确状态并展示下一题按钮
+                setOptionStatus({
+                  option: props.questions[currentQuestionIndex].question_answer,
+                  status: "right"
+                });
+                setShowNext(true);
               } else {
                 // 回答错误
-                // 将该选项设置为唯一错误状态
-                const temStatus = {};
-                temStatus[v.option] = false;
-                setOptionStatus(temStatus);
+                // 将该选项设置为错误状态
+                setOptionStatus({ option: v.option, status: "wrong" });
+                setShowNext(false);
               }
             }}
           >
             {v.option}
           </AtButton>
         ))}
+      </View>
+      <View>
+        {showNext && <AtButton onClick={handleNext}>下一道题</AtButton>}
       </View>
     </View>
   );
